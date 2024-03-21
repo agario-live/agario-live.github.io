b57e4c149464ea2590166f92079be70d66973f62
myagariogames <107417484+myagariogames@users.noreply.github.com>
2024-03-21 23:57:23 +0300

    e

---
+++ adex.js
@@ -0,0 +1,4 @@
+var e=document.createElement('div');
+e.id='hXjAMPCBgqHb';
+e.style.display='none';
+document.body.appendChild(e);
---
+++ skins.js
@@ -0,0 +1,213 @@
+document.getElementById("idSkinList").innerHTML = `
+		<div class="mlartb-skin-box">
+			<img class="skin_img" onclick="setSkinListClick('noskin')" rel="noskin" src="./skins/noskin.png">
+		</div>
+		<div class="mlartb-skin-box">
+			<img class="skin_img" onclick="setSkinListClick('1')" rel="1" src="./skins/1.png">
+		</div>
+		<div class="mlartb-skin-box">
+			<img class="skin_img" onclick="setSkinListClick('n2')" rel="2" src="./skins/2.png">
+		</div>
+		<div class="mlartb-skin-box">
+			<img class="skin_img" onclick="setSkinListClick('3')" rel="3" src="./skins/3.png">
+		</div>
+		<div class="mlartb-skin-box">
+			<img class="skin_img" onclick="setSkinListClick('4')" rel="4" src="./skins/4.png">
+		</div>
+		<div class="mlartb-skin-box">
+			<img class="skin_img" onclick="setSkinListClick('5')" rel="5" src="./skins/5.png">
+		</div>
+		<div class="mlartb-skin-box">
+			<img class="skin_img" onclick="setSkinListClick('6')" rel="6" src="./skins/6.png">
+		</div>
+		<div class="mlartb-skin-box">
+			<img class="skin_img" onclick="setSkinListClick('7')" rel="7" src="./skins/7.png">
+		</div>
+		<div class="mlartb-skin-box">
+			<img class="skin_img" onclick="setSkinListClick('8')" rel="8" src="./skins/8.png">
+		</div>
+		<div class="mlartb-skin-box">
+			<img class="skin_img" onclick="setSkinListClick('9')" rel="9" src="./skins/9.png">
+		</div>
+		<div class="mlartb-skin-box">
+			<img class="skin_img" onclick="setSkinListClick('10')" rel="10" src="./skins/10.png">
+		</div>
+		<div class="mlartb-skin-box">
+			<img class="skin_img" onclick="setSkinListClick('11')" rel="11" src="./skins/11.png">
+		</div>
+		<div class="mlartb-skin-box">
+			<img class="skin_img" onclick="setSkinListClick('12')" rel="12" src="./skins/12.png">
+		</div>
+		<div class="mlartb-skin-box">
+			<img class="skin_img" onclick="setSkinListClick('13')" rel="13" src="./skins/13.png">
+		</div>
+		<div class="mlartb-skin-box">
+			<img class="skin_img" onclick="setSkinListClick('14')" rel="14" src="./skins/14.png">
+		</div>
+		<div class="mlartb-skin-box">
+			<img class="skin_img" onclick="setSkinListClick('15')" rel="15" src="./skins/15.png">
+		</div>
+		<div class="mlartb-skin-box">
+			<img class="skin_img" onclick="setSkinListClick('16')" rel="16" src="./skins/16.png">
+		</div>
+		<div class="mlartb-skin-box">
+			<img class="skin_img" onclick="setSkinListClick('17')" rel="17" src="./skins/17.png">
+		</div>
+		<div class="mlartb-skin-box">
+			<img class="skin_img" onclick="setSkinListClick('18')" rel="18" src="./skins/18.png">
+		</div>
+		<div class="mlartb-skin-box">
+			<img class="skin_img" onclick="setSkinListClick('19')" rel="19" src="./skins/19.png">
+		</div>
+		<div class="mlartb-skin-box">
+			<img class="skin_img" onclick="setSkinListClick('20')" rel="20" src="./skins/20.png">
+		</div>
+		<div class="mlartb-skin-box">
+			<img class="skin_img" onclick="setSkinListClick('21')" rel="21" src="./skins/21.png">
+		</div>
+		<div class="mlartb-skin-box">
+			<img class="skin_img" onclick="setSkinListClick('22')" rel="22" src="./skins/22.png">
+		</div>
+		<div class="mlartb-skin-box">
+			<img class="skin_img" onclick="setSkinListClick('23')" rel="23" src="./skins/23.png">
+		</div>
+		<div class="mlartb-skin-box">
+			<img class="skin_img" onclick="setSkinListClick('24')" rel="24" src="./skins/24.png">
+		</div>
+		<div class="mlartb-skin-box">
+			<img class="skin_img" onclick="setSkinListClick('25')" rel="25" src="./skins/25.png">
+		</div>
+		<div class="mlartb-skin-box">
+			<img class="skin_img" onclick="setSkinListClick('26')" rel="26" src="./skins/26.png">
+		</div>
+		<div class="mlartb-skin-box">
+			<img class="skin_img" onclick="setSkinListClick('27')" rel="27" src="./skins/27.png">
+		</div>
+		<div class="mlartb-skin-box">
+			<img class="skin_img" onclick="setSkinListClick('28')" rel="28" src="./skins/28.png">
+		</div>
+		<div class="mlartb-skin-box">
+			<img class="skin_img" onclick="setSkinListClick(29')" rel="29" src="./skins/29.png">
+		</div>
+		<div class="mlartb-skin-box">
+			<img class="skin_img" onclick="setSkinListClick('30')" rel="30" src="./skins/30.png">
+		</div>
+		<div class="mlartb-skin-box">
+			<img class="skin_img" onclick="setSkinListClick('31')" rel="31" src="./skins/31.png">
+		</div>
+		<div class="mlartb-skin-box">
+			<img class="skin_img" onclick="setSkinListClick('32')" rel="32" src="./skins/32.png">
+		</div>
+		<div class="mlartb-skin-box">
+			<img class="skin_img" onclick="setSkinListClick('33')" rel="33" src="./skins/33.png">
+		</div>
+		<div class="mlartb-skin-box">
+			<img class="skin_img" onclick="setSkinListClick('34')" rel="34" src="./skins/34.png">
+		</div>
+		<div class="mlartb-skin-box">
+			<img class="skin_img" onclick="setSkinListClick('35')" rel="35" src="./skins/35.png">
+		</div>
+		<div class="mlartb-skin-box">
+			<img class="skin_img" onclick="setSkinListClick('36')" rel="36" src="./skins/36.png">
+		</div>
+		<div class="mlartb-skin-box">
+			<img class="skin_img" onclick="setSkinListClick('37')" rel="37" src="./skins/37.png">
+		</div>
+		<div class="mlartb-skin-box">
+			<img class="skin_img" onclick="setSkinListClick('37')" rel="37" src="./skins/37.png">
+		</div>
+		<div class="mlartb-skin-box">
+			<img class="skin_img" onclick="setSkinListClick('38)" rel="38" src="./skins/38.png">
+		</div>
+		<div class="mlartb-skin-box">
+			<img class="skin_img" onclick="setSkinListClick('39')" rel="39" src="./skins/39.png">
+		</div>
+		<div class="mlartb-skin-box">
+			<img class="skin_img" onclick="setSkinListClick('40')" rel="40" src="./skins/40.png">
+		</div>
+		<div class="mlartb-skin-box">
+			<img class="skin_img" onclick="setSkinListClick('41')" rel="41" src="./skins/41.png">
+		</div>
+		<div class="mlartb-skin-box">
+			<img class="skin_img" onclick="setSkinListClick('42')" rel="42" src="./skins/42.png">
+		</div>
+		<div class="mlartb-skin-box">
+			<img class="skin_img" onclick="setSkinListClick('43')" rel="43" src="./skins/43.png">
+		</div>
+		<div class="mlartb-skin-box">
+			<img class="skin_img" onclick="setSkinListClick('44')" rel="44" src="./skins/44.png">
+		</div>
+		<div class="mlartb-skin-box">
+			<img class="skin_img" onclick="setSkinListClick('45')" rel="45" src="./skins/45.png">
+		</div>
+		<div class="mlartb-skin-box">
+			<img class="skin_img" onclick="setSkinListClick('46')" rel="46" src="./skins/46.png">
+		</div>
+		<div class="mlartb-skin-box">
+			<img class="skin_img" onclick="setSkinListClick('47')" rel="47" src="./skins/47.png">
+		</div>
+		<div class="mlartb-skin-box">
+			<img class="skin_img" onclick="setSkinListClick('48')" rel="48" src="./skins/48.png">
+		</div>
+		<div class="mlartb-skin-box">
+			<img class="skin_img" onclick="setSkinListClick('49')" rel="49" src="./skins/49.png">
+		</div>
+		<div class="mlartb-skin-box">
+			<img class="skin_img" onclick="setSkinListClick('50')" rel="50" src="./skins/50.png">
+		</div>
+    `
+	
+	/*<ul class="skinList">
+            <li rel="noskin"><img class="skin_img" onclick="setSkinListClick('noskin')" rel="noskin" src="./skins/noskin.png"></li>
+            <li rel="1"><img class="skin_img" onclick="setSkinListClick('1')" rel="1" src="./skins/1.png"></li>
+            <li rel="2"><img class="skin_img" onclick="setSkinListClick('2')" rel="2" src="./skins/2.png"></li>
+            <li rel="3"><img class="skin_img" onclick="setSkinListClick('3')" rel="3" src="./skins/3.png"></li>
+            <li rel="4"><img class="skin_img" onclick="setSkinListClick('4')" rel="4" src="./skins/4.png"></li>
+            <li rel="5"><img class="skin_img" onclick="setSkinListClick('5')" rel="5" src="./skins/5.png"></li>
+            <li rel="6"><img class="skin_img" onclick="setSkinListClick('6')" rel="6" src="./skins/6.png"></li>
+            <li rel="7"><img class="skin_img" onclick="setSkinListClick('7')" rel="7" src="./skins/7.png"></li>
+            <li rel="8"><img class="skin_img" onclick="setSkinListClick('8')" rel="8" src="./skins/8.png"></li>
+            <li rel="9"><img class="skin_img" onclick="setSkinListClick('9')" rel="9" src="./skins/9.png"></li>
+            <li rel="10"><img class="skin_img" onclick="setSkinListClick('10')" rel="10" src="./skins/10.png"></li>
+            <li rel="11"><img class="skin_img" onclick="setSkinListClick('11')" rel="11" src="./skins/11.png"></li>
+            <li rel="12"><img class="skin_img" onclick="setSkinListClick('12')" rel="12" src="./skins/12.png"></li>
+            <li rel="13"><img class="skin_img" onclick="setSkinListClick('13')" rel="13" src="./skins/13.png"></li>
+            <li rel="14"><img class="skin_img" onclick="setSkinListClick('14')" rel="14" src="./skins/14.png"></li>
+            <li rel="15"><img class="skin_img" onclick="setSkinListClick('15)" rel="15" src="./skins/15.png"></li>
+            <li rel="16"><img class="skin_img" onclick="setSkinListClick('16')" rel="16" src="./skins/16.png"></li>
+            <li rel="17"><img class="skin_img" onclick="setSkinListClick('17')" rel="17" src="./skins/17.png"></li>
+            <li rel="18"><img class="skin_img" onclick="setSkinListClick('18')" rel="18" src="./skins/18.png"></li>
+            <li rel="19"><img class="skin_img" onclick="setSkinListClick('19')" rel="19" src="./skins/19.png"></li>
+            <li rel="20"><img class="skin_img" onclick="setSkinListClick('20')" rel="20" src="./skins/20.png"></li>
+            <li rel="21"><img class="skin_img" onclick="setSkinListClick('21')" rel="21" src="./skins/21.png"></li>
+            <li rel="22"><img class="skin_img" onclick="setSkinListClick('22')" rel="22" src="./skins/22.png"></li>
+            <li rel="23"><img class="skin_img" onclick="setSkinListClick('23')" rel="23" src="./skins/23.png"></li>
+            <li rel="24"><img class="skin_img" onclick="setSkinListClick('24')" rel="24" src="./skins/24.png"></li>
+            <li rel="25"><img class="skin_img" onclick="setSkinListClick('25')" rel="25" src="./skins/25.png"></li>
+            <li rel="26"><img class="skin_img" onclick="setSkinListClick('26')" rel="26" src="./skins/26.png"></li>
+            <li rel="27"><img class="skin_img" onclick="setSkinListClick('27')" rel="27" src="./skins/27.png"></li>
+            <li rel="28"><img class="skin_img" onclick="setSkinListClick('28')" rel="28" src="./skins/28.png"></li>
+            <li rel="29"><img class="skin_img" onclick="setSkinListClick('29')" rel="29" src="./skins/29.png"></li>
+            <li rel="30"><img class="skin_img" onclick="setSkinListClick('30')" rel="30" src="./skins/30.png"></li>
+			<li rel="31"><img class="skin_img" onclick="setSkinListClick('31')" rel="31" src="./skins/31.png"></li>
+            <li rel="32"><img class="skin_img" onclick="setSkinListClick('32')" rel="32" src="./skins/32.png"></li>
+            <li rel="33"><img class="skin_img" onclick="setSkinListClick('33')" rel="33" src="./skins/33.png"></li>
+            <li rel="34"><img class="skin_img" onclick="setSkinListClick('34')" rel="34" src="./skins/34.png"></li>
+            <li rel="35"><img class="skin_img" onclick="setSkinListClick('35')" rel="35" src="./skins/35.png"></li>
+            <li rel="36"><img class="skin_img" onclick="setSkinListClick('36')" rel="36" src="./skins/36.png"></li>
+            <li rel="37"><img class="skin_img" onclick="setSkinListClick('37')" rel="37" src="./skins/37.png"></li>
+            <li rel="38"><img class="skin_img" onclick="setSkinListClick('38')" rel="38" src="./skins/38.png"></li>
+            <li rel="39"><img class="skin_img" onclick="setSkinListClick('39')" rel="9" src="./skins/39.png"></li>
+            <li rel="40"><img class="skin_img" onclick="setSkinListClick('40')" rel="40" src="./skins/40.png"></li>
+            <li rel="41"><img class="skin_img" onclick="setSkinListClick('41')" rel="41" src="./skins/41.png"></li>
+            <li rel="42"><img class="skin_img" onclick="setSkinListClick('42')" rel="42" src="./skins/42.png"></li>
+            <li rel="43"><img class="skin_img" onclick="setSkinListClick('43')" rel="43" src="./skins/43.png"></li>
+            <li rel="44"><img class="skin_img" onclick="setSkinListClick('44')" rel="44" src="./skins/44.png"></li>
+            <li rel="45"><img class="skin_img" onclick="setSkinListClick('45)" rel="45" src="./skins/45.png"></li>
+            <li rel="46"><img class="skin_img" onclick="setSkinListClick('46')" rel="46" src="./skins/46.png"></li>
+            <li rel="47"><img class="skin_img" onclick="setSkinListClick('47')" rel="47" src="./skins/47.png"></li>
+            <li rel="48"><img class="skin_img" onclick="setSkinListClick('48')" rel="48" src="./skins/48.png"></li>
+            <li rel="49"><img class="skin_img" onclick="setSkinListClick('49')" rel="49" src="./skins/49.png"></li>
+            <li rel="50"><img class="skin_img" onclick="setSkinListClick('50')" rel="50" src="./skins/50.png"></li>
+            
+           </ul>*/
